6ccede8bad31f94e14f8c2267c23cda686d63383
jsbin0526 <jsbin0526@gmail.com>
2023-05-15 21:19:08 +0900

    add menu_toggle_button

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import Router from './Router.js';
 import GamePage from './Pages/GamePage.js';
 import ProductionPage from './Pages/ProductionPage.js';
 
-const paths = {'#game': new GamePage, '#production': new ProductionPage}
+const paths = {'#game': new GamePage(), '#production': new ProductionPage()}
 const router = new Router(paths);
 
-const menu_toggle_button = $(".menu-toggle");
+const button_icon = $(".menu-toggle > img");
 const menu = $(".menu");
-let menu_toggle = menu_toggle_button.css('display') == "span";
-menu_toggle_button.on('click', () => {
+let menu_toggle = true;
+button_icon.on('click', () => {
+    console.log(menu_toggle);
     menu_toggle = !menu_toggle;
+    button_icon.attr('src', menu_toggle ? "hamburger_button.svg" : "close_button.svg");
+    menu.css('display', menu_toggle ? "none" : "block");
 });
