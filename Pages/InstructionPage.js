e83357f0fba3f69826f17c329d7835adf2219b00
jsbin0526 <jsbin0526@gmail.com>
2023-06-02 09:05:26 +0900

    developing production page...

--- Pages/InstructionPage.js
+++ Pages/InstructionPage.js
@@ -1,32 +1,32 @@
 export default class InstructionPage {
     render() {
         return /*html*/`
-            <div class="instruction">
-                <img src="../profile.png" class="profile-image">
+        <div class="instruction">
+            <img src="../profile.png" class="profile-image">
+            <div>
                 <div>
-                    <div>
-                        <h2>Hi! My name is Seonbin Ji!</h2>
-                        <p>I am majoring in the department of Computer Engineering in Kangwon National University.</p>
-                        <p>I am going to enlist korean army after finishing first semester.
-                    </div>
-                    <br/>
-                    <div>
-                        <h2>Interests</h2>
-                        <ul>
-                            <li>Problem Solving with Python</li>
-                            <li>Web Programming 😃</li>
-                            <li>System Programming 😂</li>
-                            <li>Mobile Programming 🤔</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>Links</h2>
-                        <ul style="list-style:none;padding-left:0px">
-                            <li style="display:inline;"><a href="https://github.com/jsbin0526"><img src="../github-mark.svg"></a></li>
-                            <li style="display:inline;"><a href="https://www.acmicpc.net/user/jsbin0526"><img width="98px" src="../baekjoon-mark.svg"></a></li>
-                        </ul>
-                    </div>
+                    <h2>Hi! My name is Seonbin Ji!</h2>
+                    <p>I am majoring in the department of Computer Engineering in Kangwon National University.</p>
+                    <p>I am going to enlist korean army after finishing first semester.
                 </div>
+                <br/>
+                <div>
+                    <h2>Interests</h2>
+                    <ul>
+                        <li>Problem Solving with Python</li>
+                        <li>Web Programming 😃</li>
+                        <li>System Programming 😂</li>
+                        <li>Mobile Programming 🤔</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Links</h2>
+                    <ul style="list-style:none;padding-left:0px">
+                        <li style="display:inline;"><a href="https://github.com/jsbin0526"><img src="../github-mark.svg"></a></li>
+                        <li style="display:inline;"><a href="https://www.acmicpc.net/user/jsbin0526"><img width="98px" src="../baekjoon-mark.svg"></a></li>
+                    </ul>
+                </div>
+            </div>
         `;
     }
 }
--- Pages/ProductionPage.js
+++ Pages/ProductionPage.js
@@ -1,30 +1,23 @@
 export default class ProductionPage {
+    navigate(year) {
+        
+    }
+
     render() {
         return /*html*/`
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
-            <h1>This is Production Page.</h1>
+        <h3>Year</h3>
+        <nav>
+            <ul class="pagination">
+                <li class="page-item active"><a class="page-link">All</a></li>
+                <li class="page-item"><a class="page-link">2022</a></li>
+                <li class="page-item"><a class="page-link">2021</a></li>
+                <li class="page-item"><a class="page-link">2020</a></li>
+            </ul>
+        </nav>
+        <h3>2022</h3>
+        <ul>
+            <li><a href="https://www.codeduri.kangwon.ac.kr">코드두리(서버 작동 안함)</a></li>
+        </ul>
         `;
-    }
+    } 
 }
